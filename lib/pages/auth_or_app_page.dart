8d94a1f04c621ae556eb9f3d091fc3f4e64ade1f
christianomere <christianomere@gmail.com>
2022-01-01 14:41:03 +0000

    Firebase initialized

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:paapiar/core/models/paapiar_user.dart';
 import 'package:paapiar/core/services/auth/auth_mock_service.dart';
 import 'package:paapiar/pages/auth_page.dart';
@@ -8,20 +10,30 @@ import 'package:paapiar/pages/loading_page.dart';
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({Key? key}) : super(key: key);
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<PaapiarUser?>(
-
-        stream: AuthMockService().userChanges,
-        builder: (ctx, snapshot){
-          if(snapshot.connectionState == ConnectionState.waiting){
-            return LoadingPage();
-          } else{
-            return snapshot.hasData ? ChatPage() : AuthPage();
-          }
-        },
-      ),
-    );
+     return FutureBuilder(
+       future: init(context),
+       builder:(ctx, snapshot) {
+         if(snapshot.connectionState == ConnectionState.waiting){
+           return LoadingPage();
+         }else{
+           return  StreamBuilder<PaapiarUser?>(
+             stream: AuthMockService().userChanges,
+             builder: (ctx, snapshot){
+               if(snapshot.connectionState == ConnectionState.waiting){
+                 return LoadingPage();
+               } else{
+                 return snapshot.hasData ? ChatPage() : AuthPage();
+               }
+             },
+           );
+         }
+       },
+     );
   }
 }
