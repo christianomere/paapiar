0cf7e0477a0436acd70c6bf0becf65b275dd0b50
christianomere <christianomere@gmail.com>
2022-01-04 16:03:09 +0000

    Firebase Notifications working on Android.

--- lib/core/services/notification/chat_notification_service.dart
+++ lib/core/services/notification/chat_notification_service.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:paapiar/core/models/chat_notification.dart';
 
@@ -21,4 +22,47 @@ class ChatNotificationService with ChangeNotifier {
     _items.removeAt(i);
     notifyListeners();
   }
+
+  // Push Notification
+  Future<void> init() async {
+    await _configureTerminated();
+    await _configureForeground();
+    await _configureBackground();
+  }
+
+  Future<bool> get _isAuthorized async {
+    final messaging = FirebaseMessaging.instance;
+    final settings = await messaging.requestPermission();
+    return settings.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessage.listen(_messageHandler);
+    }
+  }
+
+  Future<void> _configureBackground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessageOpenedApp.listen(_messageHandler);
+    }
+  }
+
+  Future<void> _configureTerminated() async {
+    if (await _isAuthorized) {
+      RemoteMessage? initialMsg =
+      await FirebaseMessaging.instance.getInitialMessage();
+      _messageHandler(initialMsg);
+    }
+  }
+
+  void _messageHandler(RemoteMessage? msg) {
+    if (msg == null || msg.notification == null) return;
+
+    add(ChatNotification(
+      title: msg.notification!.title ?? 'Não informado!',
+      body: msg.notification!.body ?? 'Não informado!',
+    ));
+  }
 }
+
--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:paapiar/core/models/paapiar_user.dart';
 import 'package:paapiar/core/services/auth/auth_service.dart';
+import 'package:paapiar/core/services/notification/chat_notification_service.dart';
+import 'package:provider/provider.dart';
 
 import 'auth_page.dart';
 import 'chat_page.dart';
@@ -12,7 +14,10 @@ class AuthOrAppPage extends StatelessWidget {
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
-
+    await Provider.of<ChatNotificationService>(
+      context,
+      listen: false,
+    ).init();
   }
 
   @override
